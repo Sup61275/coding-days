8f016923dba54460ccc3efe59ba6c930eb32fe34
Supriya Kashyap <kashyapsupriya61275@gmail.com>
2023-08-12 16:50:58 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (80.26%) - LeetSync

--- 61-rotate-list/rotate-list.java
+++ 61-rotate-list/rotate-list.java
@@ -10,48 +10,25 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if (head == null || head.next == null || k == 0) {
+        if(head==null||k==0){
             return head;
         }
-        
-        int length = getLength(head);
-        k %= length;  // Adjust k to be within the length of the list
-        
-        if (k == 0) {
-            return head;
-        }
-        
-        ListNode newHead = findNewHead(head, length - k);
-        ListNode newTail = findNewTail(newHead);
-        
-        newTail.next = head;
-        head = newHead;
-        return head;
-    }
-    
-    private int getLength(ListNode head) {
-        int length = 0;
-        while (head != null) {
-            head = head.next;
-            length++;
-        }
-        return length;
-    }
-    
-    private ListNode findNewHead(ListNode head, int newHeadIndex) {
-        for (int i = 1; i < newHeadIndex; i++) {
-            head = head.next;
-        }
-        ListNode newHead = head.next;
-        head.next = null;
-        return newHead;
-    }
-    
-    private ListNode findNewTail(ListNode head) {
-        while (head.next != null) {
-            head = head.next;
-        }
-        return head;
+       ListNode current=head;
+       int count=1;
+       while(current.next!=null){
+           current=current.next;
+           count++;
+       }
+       current.next=head;
+       current=head;
+       k%=count;
+       for(int i=1;i<count-k;i++){
+           current=current.next;
+       }
+       ListNode newHead=current.next;
+          current.next=null;
+          return newHead;
+
     }
 }
 
