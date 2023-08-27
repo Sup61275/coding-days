99356f7856f2cca376edc7c9acb6bb51aec641be
Supriya Kashyap <kashyapsupriya61275@gmail.com>
2023-08-27 13:49:31 +0530

    Time: 1 ms (64.24%) | Memory: 42.7 MB (95.61%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -18,16 +18,19 @@ class Solution {
         if (root == null) {
             return true;
         }
-        if (Math.abs(balance(root.left) - balance(root.right)) > 1) {
+        if (Math.abs(height(root.left) - height(root.right)) > 1) {
             return false;
         }
         return isBalanced(root.left) && isBalanced(root.right);
     }
 
-    private int balance(TreeNode root) {
+    private int height(TreeNode root) {
         if (root == null) {
             return 0;
         }
-        return 1 + Math.max(balance(root.left), balance(root.right));
+       int leftheight=height(root.left);
+       int rightheight=height(root.right);
+       int myheight=Math.max(leftheight,rightheight)+1;
+       return myheight;
     }
 }
