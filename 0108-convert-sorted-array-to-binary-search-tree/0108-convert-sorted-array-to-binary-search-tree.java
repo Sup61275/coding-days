927125710126810e9b547e3675d7dbcc7f5c9f08
Supriya Kashyap <kashyapsupriya61275@gmail.com>
2023-10-31 20:08:41 +0530

    Time: 0 ms (100%), Space: 43 MB (69.92%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.java
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.java
@@ -18,15 +18,7 @@ class Solution {
         return tree(nums, 0, nums.length - 1);
     }
 
-    private int height(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        int leftHeight = height(root.left);
-        int rightHeight = height(root.right);
-        int myHeight = Math.max(leftHeight, rightHeight) + 1;
-        return myHeight;
-    }
+   
 
     private TreeNode tree(int[] nums, int left, int right) {
         if (left > right) {
