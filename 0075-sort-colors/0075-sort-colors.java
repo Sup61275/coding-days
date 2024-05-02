ed2533d7b1b414773201bc950d0bd21b64165ce2
Supriya Kashyap <kashyapsupriya61275@gmail.com>
2024-05-02 23:22:28 +0530

    Time: 1 ms (22.44%), Space: 41.9 MB (19.36%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,6 +1,43 @@
+import java.util.HashMap;
+
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int count1 = 0;
+        int count2 = 0;
+        int count3 = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
         
+        for (int i = 0; i < nums.length; i++) {
+            if (!map.containsKey(nums[i])) {
+                map.put(nums[i], 1);
+            } else {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            }
+        }
+        
+        if (map.containsKey(0)) {
+            count1 = map.get(0);
+        }
+        
+        if (map.containsKey(1)) {
+            count2 = map.get(1);
+        }
+        
+        if (map.containsKey(2)) {
+            count3 = map.get(2);
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (count1 > 0) {
+                nums[i] = 0;
+                count1--;
+            } else if (count2 > 0) {
+                nums[i] = 1;
+                count2--;
+            } else {
+                nums[i] = 2;
+                count3--;
+            }
+        }
     }
 }
