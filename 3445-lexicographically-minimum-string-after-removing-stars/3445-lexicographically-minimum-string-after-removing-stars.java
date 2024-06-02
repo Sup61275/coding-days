b7fcaf7b9491f02ba9d66fa87e4c105facde573a
Supriya Kashyap <kashyapsupriya61275@gmail.com>
2024-06-02 13:47:05 +0530

    Time: 114 ms (100%), Space: 46.4 MB (100%) - LeetHub

--- 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.java
+++ 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.java
@@ -1,22 +1,20 @@
 class Solution {
     public String clearStars(String s) {
-      StringBuilder sb = new StringBuilder();
-        PriorityQueue<Character> pq = new PriorityQueue<>();
-
-        for (char c : s.toCharArray()) {
-            if (c != '*') {
-                sb.append(c);
-            } else {
-                if (!pq.isEmpty()) {
-                    char removedChar = pq.poll(); 
-                    sb.deleteCharAt(sb.lastIndexOf(Character.toString(removedChar))); 
-                }
-            }
-            if (c != '*') {
-                pq.offer(c);
+    StringBuilder sb= new StringBuilder();
+    PriorityQueue<Character>pq= new PriorityQueue<>();
+    for(char c:s.toCharArray()){
+        if(c!='*'){
+            sb.append(c);
+        }else{
+            if(!pq.isEmpty()){
+                char removedchar=pq.poll();
+                sb.deleteCharAt(sb.lastIndexOf(Character.toString(removedchar)));
             }
         }
-
-        return sb.toString();
+        if(c!='*'){
+            pq.offer(c);
+        }
+    }
+    return sb.toString();
     }
 }
