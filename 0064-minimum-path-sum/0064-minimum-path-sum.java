c92c1aeda4133dd766214f21c11c19f4e9409c31
Supriya Kashyap <kashyapsupriya61275@gmail.com>
2024-06-19 01:50:20 +0530

    Time: 3 ms (66.56%), Space: 45.5 MB (36.51%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,27 +1,26 @@
 class Solution {
-public int minPathSum(int[][] grid) {
-    int [][]memo=new int[201][201];
-    for(int[]row:memo){
-        Arrays.fill(row,-1);
-    }
-        return solve(grid,0,0,memo);
-    }
-    int solve(int[][]grid,int i,int j, int[][]memo){
-      int m= grid.length;
-      int n=grid[0].length;
-      if(i>=m||j>=n){
-        return Integer.MAX_VALUE;
-      }
-      if(memo[i][j]!=-1){
-        return memo[i][j];
-      }
-      if(i==m-1 && j==n-1){
-        return grid[i][j];
-      }
-      
-      int right=solve(grid,i,j+1,memo);
-    
-      int down=solve(grid,i+1,j,memo);
-      return memo[i][j]=grid[i][j]+Math.min(right,down);
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+
+        dp[0][0] = grid[0][0];
+
+        // Fill the first column
+        for (int row = 1; row < m; row++) {
+            dp[row][0] = grid[row][0] + dp[row - 1][0];
+        }
+        // Fill the first row
+        for (int col = 1; col < n; col++) {
+            dp[0][col] = grid[0][col] + dp[0][col - 1];
+        }
+        // Fill the rest of the dp array
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = grid[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+
+        return dp[m - 1][n - 1];
     }
 }
