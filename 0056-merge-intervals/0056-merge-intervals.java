7c387dbe2050ed12d93827f56ee814d8b6bef96c
Supriya Kashyap <kashyapsupriya61275@gmail.com>
2024-06-21 16:17:58 +0530

    Time: 213 ms (5.08%), Space: 46.5 MB (31.93%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -6,23 +6,15 @@ class Solution {
         }
         Arrays.sort(intervals,Comparator.comparingInt(a->a[0]));
         int index=0;
-        int[][]ans= new int[n][2];
-        ans[index][0]=intervals[0][0];
-        ans[index][1]=intervals[0][1];
-        index++;
         for(int i=1;i<n;i++){
-           int u=ans[index-1][1];
-           int v=intervals[i][0];
-           if(u>=v){
-            ans[index-1][1]=Math.max(ans[index-1][1],intervals[i][1]);
-            
-            
-           }else{
-           ans[index][0]=intervals[i][0];
-           ans[index][1]=intervals[i][1];
-           index++;
-           }
+            if(intervals[i][0]<=intervals[index][1]){
+                intervals[index][1]=Math.max( intervals[index][1],intervals[i][1]);
+            }else{
+                index++;
+                intervals[index]=intervals[i];
+              
+            }
         }
-        return Arrays.copyOf(ans,index);
+        return Arrays.copyOf(intervals,index+1);
     }
 }
