6bec33556d4f535d77532eaffbd2ea4f49e37194
Supriya Kashyap <kashyapsupriya61275@gmail.com>
2024-06-22 01:56:02 +0530

    Time: 4 ms (12.38%), Space: 44.9 MB (33.16%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,25 +1,37 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        int n=intervals.length;
-        List<int[]>result= new ArrayList<>();
-        int i=0;
-        while(i<n){
-            if(intervals[i][1]<newInterval[0]){
-                result.add(intervals[i]);
-            }else if(intervals[i][0] > newInterval[1]){
-                break;
-            }else{//merge intervals
-                newInterval[0]=Math.min(newInterval[0],intervals[i][0]);
-                newInterval[1]=Math.max(newInterval[1],intervals[i][1]);
-            }
-            i++;
-        }
-        result.add(newInterval);
-        while(i<n){
-            result.add(intervals[i]);
-            i++;
-        }
+         int n = intervals.length;
+        int[][] newIntervals = new int[n + 1][2];
 
-        return result.toArray(new int[result.size()][]);
+        // Copy all existing intervals to newIntervals
+        for (int i = 0; i < n; i++) {
+            newIntervals[i] = intervals[i];
+        }
+        
+        // Add the new interval to newIntervals
+        newIntervals[n] = newInterval;
+        
+        // Merge intervals
+        return merge(newIntervals);
+    }
+       
+       
+    public int[][] merge(int[][] intervals) {
+        int n=intervals.length;
+        if(n<=1){
+            return intervals;
+        }
+        Arrays.sort(intervals,Comparator.comparingInt(a->a[0]));
+        int index=0;
+        for(int i=1;i<n;i++){
+            if(intervals[i][0]<=intervals[index][1]){
+                intervals[index][1]=Math.max( intervals[index][1],intervals[i][1]);
+            }else{
+                index++;
+                intervals[index]=intervals[i];
+              
+            }
+        }
+        return Arrays.copyOf(intervals,index+1);
     }
 }
