f8dd392869da3499c1955e66f814abbef04dc2ad
Supriya Kashyap <kashyapsupriya61275@gmail.com>
2024-07-10 13:39:38 +0530

    Time: 15 ms (16.62%), Space: 44.7 MB (25.16%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,56 +1,22 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        Map<Character, Integer> row_map = new HashMap<>();
-        Map<Character, Integer> column_map = new HashMap<>();
-        Map<Character, Integer> box_map = new HashMap<>();
-
-        for (int i = 0; i < 9; i++) {
-            row_map.clear();
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] != '.') {
-                    row_map.put(board[i][j], row_map.getOrDefault(board[i][j], 0) + 1);
-                }
-            }
-            for (int itr : row_map.values()) {
-                if (itr > 1) {
-                    return false;
-                }
-            }
-        }
-        for (int i = 0; i < 9; i++) {
-            column_map.clear();
-            for (int j = 0; j < 9; j++) {
-                if (board[j][i] != '.') {
-                    column_map.put(board[j][i], column_map.getOrDefault(board[j][i], 0) + 1);
-                }
-            }
-            for (int itr : column_map.values()) {
-                if (itr > 1) {
-                    return false;
-                }
-            }
-        }
-  
-        for (int row = 0; row < 9; row += 3) {
-        for (int col = 0; col < 9; col += 3) {
-            box_map.clear();
-            for (int i = row; i < row + 3; i++) {
-            for (int j = col; j < col + 3; j++) {
-                if (board[i][j] != '.') {
-                    box_map.put(board[i][j], box_map.getOrDefault(board[i][j], 0) + 1);
-                    }
-                }
-            }
-            for (int itr : box_map.values()) {
-                if (itr > 1) {
+      Set<String>set= new HashSet<>();
+      for(int i=0;i<9;i++){
+        for(int j=0;j<9;j++){
+            if(board[i][j]=='.')continue;
+            String row=board[i][j]+"row"+i;
+            String col=board[i][j]+"col"+j;
+            String box=board[i][j]+"box"+(i/3)+""+(j/3);
+            if(set.contains(row)||set.contains(col)||set.contains(box)){
                 return false;
-                }
             }
+            set.add(row);
+            set.add(col);
+            set.add(box);
         }
-        
+      }
+      return true;
     }
-    return true;
-}
 }
 
 
