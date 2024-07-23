831a25f21ba72d1666800c112f1875f136805fbc
Supriya Kashyap <kashyapsupriya61275@gmail.com>
2024-07-23 23:25:45 +0530

    Time: 0 ms (100%), Space: 44.5 MB (39.98%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -16,7 +16,7 @@ public class Solution {
     }
     ListNode slow=head;
     ListNode fast=head;
-    while(fast!=null&& fast.next!=null){
+    while(fast!=null && fast.next!=null){
         slow=slow.next;
         fast=fast.next.next;
         if(slow==fast){
@@ -26,12 +26,11 @@ public class Solution {
     if(fast==null||fast.next==null){
         return null;
     }
-    
     slow=head;
     while(slow!=fast){
         slow=slow.next;
         fast=fast.next;
-        
+       
     }
     return slow;
     }
